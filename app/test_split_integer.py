a9437a28a8318763fb4d059018d98639979e87ab
unknown <kgoncharenko83@gmail.com>
2026-02-17 17:19:51 +0100

    Added tests for length func and correct value

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -6,7 +6,7 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    assert split_integer(10, 2) == [5, 5]
+    assert split_integer(12, 3) == [4, 4, 4]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
@@ -19,3 +19,10 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     assert split_integer(5, 6) == [0, 1, 1, 1, 1, 1]
+
+def test_length_should_equal_number_of_parts() -> None:
+    assert len(split_integer(10, 2)) == 2
+
+def test_difference_between_max_and_min_should_not_exceed_one() -> None:
+    result = split_integer(10, 3)
+    assert max(result) - min(result) <= 1
